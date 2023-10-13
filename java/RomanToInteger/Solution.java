77618ecd4339d4b107c837c77fd00dbf35eba064
Christopher Junius <41166228+cjunius@users.noreply.github.com>
2023-10-13 09:40:38 -0400

    LeetCode - RomanToInteger Solution

---
+++ java/RomanToInteger/Solution.java
@@ -0,0 +1,33 @@
+class Solution {
+
+    public int romanToInt(String s) {
+
+        int ans = 0, prev = 0;
+
+        for(int i=s.length()-1; i>=0; i--) {
+           int num = getRomanNumeralValue(s.charAt(i));
+
+            if(num < prev) {
+                ans -= num;
+            } else {
+                ans += num;
+            }
+            prev = num;
+        }
+        return ans;
+
+    }
+
+    public int getRomanNumeralValue(Character c) {
+        switch(c) {
+            case('I'): return 1;
+            case('V'): return 5;
+            case('X'): return 10;
+            case('L'): return 50;
+            case('C'): return 100;
+            case('D'): return 500;
+            case('M'): return 1000;
+            default: return 0;
+        }
+    }
+}
